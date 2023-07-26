e1ea2ccf597765f708115595cffd780b5a2a63ec
cheeze2000 <me@cheeze2000.net>
2023-07-26 08:06:35 +0800

    ENG-5: Interface with git2-rs (#4)
    * build: install git2-rs
    
    * feat: command to check libgit2 version
    
    * feat: show libgit2 version in frontend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use git2::Version;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-	format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_libgit2_version() -> String {
+	let (major, minor, rev) = Version::get().libgit2_version();
+	format!("v{major}.{minor}.{rev}")
 }
 
 fn main() {
 	tauri::Builder::default()
-		.invoke_handler(tauri::generate_handler![greet])
+		.invoke_handler(tauri::generate_handler![
+			get_libgit2_version,
+		])
 		.run(tauri::generate_context!())
 		.expect("error while running tauri application");
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,18 @@
+import { invoke } from "@tauri-apps/api";
+import { Show, onMount } from "solid-js";
+
 import { useProperty } from "~/utils/useProperty";
+import { useRef } from "~/utils/useRef";
 
 const name = useProperty<string>("git.user.name");
 const email = useProperty<string>("git.user.email");
+
+const libVersion = useRef<string>("");
+
+async function init() {
+	const version: string = await invoke("get_libgit2_version");
+	libVersion(version);
+}
 
 function logout() {
 	name("");
@@ -9,9 +20,11 @@ function logout() {
 }
 
 function Home() {
+	onMount(init);
+
 	return (
 		<div class="flex justify-end items-center">
-			<div class="flex flex-col items-end p-6">
+			<div class="flex flex-col items-end p-3">
 				<p>
 					Welcome, {name()}!
 				</p>
@@ -25,6 +38,11 @@ function Home() {
 					Log out
 				</p>
 			</div>
+			<Show when={libVersion()}>
+				<div class="fixed bottom-3 left-3">
+					- Powered by libgit2 {libVersion()} -
+				</div>
+			</Show>
 		</div>
 	);
 }
