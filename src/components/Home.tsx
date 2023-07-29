4a604d7b3f0f230c08faf45f4cabe2dd7b880886
cheeze2000 <me@cheeze2000.net>
2023-07-29 19:35:12 +0800

    refactor: make welcome message fixed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -24,21 +24,18 @@ function Home() {
 
 	return (
 		<div class="flex justify-end items-center">
-			<div class="flex flex-col items-end p-3">
-				<p>
-					Welcome, {name()}!
-				</p>
-				<p>
-					{email()}
-				</p>
-				<p
-					onClick={logout}
-					class="underline cursor-pointer"
-				>
-					Log out
-				</p>
-			</div>
 			<Show when={libVersion()}>
+				<div class="fixed flex flex-col items-end top-3 right-3">
+					<span>
+						Welcome, {name()}!
+					</span>
+					<span
+						onClick={logout}
+						class="underline cursor-pointer"
+					>
+						Log out
+					</span>
+				</div>
 				<div class="fixed bottom-3 left-3">
 					- Powered by libgit2 {libVersion()} -
 				</div>
